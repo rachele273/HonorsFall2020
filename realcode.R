dfa4ed05af9d749ce618db14800399d664865e1b
rachele273 <edidi003@umn.edu>
2020-12-21 12:23:32 -0600

    commit

--- realcode.R
+++ realcode.R
@@ -6,6 +6,7 @@ install.packages("readxl")
 install.packages("reshape2")
 install.packages("factoextra")
 install.packages("ggplot2")
+install.packages("tidyLPA")
 library(ggpubr)
 library(mclust)
 library(dplyr)
@@ -13,5 +14,12 @@ library(readxl)
 library(reshape2)
 library(factoextra)
 library(ggplot2)
+library(tidyLPA)
 #imported dataset with frequencies--dataset has already been modified to contain only the columns of interest
+# carry out LPA on percent time data matrix.
+# VEI means that the clusters have variable volume, the same shape and orientation equal to coordinate axes.
+mod1 <- Mclust(BiovedaData_Edits2)
+summary(mod1)
+summary(mod1$BIC) # Mclust VEI (diagonal, equal shape) model with 2 components
+plot(mod1, what = "BIC", legendArgs = list(x = "bottomright", ncol=3))
 
