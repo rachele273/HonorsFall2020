e19c3eecf85689ee0c87ac0db8d4a27ff495e88e
rachele273 <edidi003@umn.edu>
2020-12-21 11:27:55 -0600

    Installing packages and uploading datafiles.

---
+++ realcode.R
@@ -0,0 +1,17 @@
+#using code from Dr. Warfa "Latent Profile Analysis"
+# install R packages.
+install.packages("mclust")
+install.packages("dplyr")
+install.packages("readxl")
+install.packages("reshape2")
+install.packages("factoextra")
+install.packages("ggplot2")
+library(ggpubr)
+library(mclust)
+library(dplyr)
+library(readxl)
+library(reshape2)
+library(factoextra)
+library(ggplot2)
+#imported dataset with frequencies--dataset has already been modified to contain only the columns of interest
+
